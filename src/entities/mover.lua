48183ca87a4e09f3d51d086105e937714ad6d6f4
Brad <brad@bradellis.co.uk>
2016-01-02 20:32:23 +0000

    Did a few things to mover.lua. Implemented assignFromProperties, which populates the mover's extents, speed and direction values from the properties of the Tiled object. Cleaned up some OO, added comments, and removed some redundant functions.

--- src/entities/mover.lua
+++ src/entities/mover.lua
@@ -1,22 +1,42 @@
+-------------------------------------------------------------------------------
+-- Copyright (C) Brad Ellis 2013-2016
+--
+--
+-- mover.lua
+--
+-- Movers. The big red moving blocks of death.
+-------------------------------------------------------------------------------
+
+-- The base class
 require("src/gameObject")
 
 
+
 mover = {}
-for k,v in pairs(gameObject) do
-	mover[k]=v
-end
+-- mover is the metatable for all movers, and so will contain the methods
 mover.__index = mover
 
-setmetatable(mover,
-	{__index = gameObject,
-	__call = function(cls, ...)
-		return cls.new(...)
-		end
-	})
 
-function mover.new(...)
+
+-- Constructor
+setmetatable(mover,
+	{ __index = gameObject,
+    __call = function(cls, ...)
+      return cls.new(...)
+		end
+	}
+)
+
+
+
+-------------------------------------------------------------------------------
+-- new
+--
+-- Invoked by constructor. Sets "mover" as its metatable.
+-------------------------------------------------------------------------------
+function mover.new()
 	local self = setmetatable(gameObject(), mover)
-	self:init(...)
+	self:init()
 	return self
 end
 
@@ -36,9 +56,15 @@ function mover:tex_iter()
 	end
 end
 
-function mover:init(objType)
+
+
+-------------------------------------------------------------------------------
+-- init
+--
+-- Sets default values.
+-------------------------------------------------------------------------------
+function mover:init()
 	gameObject.init(self)
-	self:setClass(objType)
 	self.size = vector(14, 14)
 	self.speed = 0
 	self.dir = vector(0, 0)
@@ -51,60 +77,165 @@ function mover:init(objType)
 	self.rayToTarget = {x = 0, y = 0}
 end
 
+
+
+-------------------------------------------------------------------------------
+-- assignFromProperties
+--
+-- Populate the mover's members with properties from its Tiled object.
+-------------------------------------------------------------------------------
 function mover:assignFromProperties(prop)
+  -- We've got many properties, some of which are needed by movers.
+  print(prop.speed)
+  self.speed = tonumber(prop.speed)
+  local extents = jSplitKV(prop.extents)
   
-   
-    return true;
+  if(prop.extents) then
+    -- If we've got only these extents then assume the mover will move from
+    -- its original position to its first extent, and then back
+    self.extents[1] = self.position:clone()
+    self.extents[2] = self.position:clone()
+    
+    self.extents[2].x = self.extents[2].x + extents.x * self.size.x
+    self.extents[2].y = self.extents[2].y + extents.y * self.size.y
+    
+    self:calcExtent(1)
+  end
+  
+  local initDir = jSplitKV(prop.initialdirection)
+  self.dir.x = initDir.x
+  self.dir.y = initDir.y
+  
+  return true
 end
 
+
+
+-------------------------------------------------------------------------------
+-- setDist
+--
+-- Set the mover's distance from its target position ("extent").
+-------------------------------------------------------------------------------
 function mover:setDist(d)
 	self.dist = d
 end
 
+
+
+-------------------------------------------------------------------------------
+-- getDist
+--
+-- Get the mover's distance from its target position ("extent").
+-------------------------------------------------------------------------------
 function mover:getDist()
 	return self.dist
 end
 
+
+
+-------------------------------------------------------------------------------
+-- getOldPos
+--
+-- Get the mover's old position (its position in the prior update frame).
+-------------------------------------------------------------------------------
 function mover:getOldPos()
 	return self.oldPos
 end
 
-function mover:setSpeed(s)
-	self.speed = s
-end
 
+-------------------------------------------------------------------------------
+-- setDir
+--
+-- Set the mover's current direction.
+-------------------------------------------------------------------------------
 function mover:setDir(dir)
 	self.dir = dir
 end
 
+
+
+-------------------------------------------------------------------------------
+-- getDir
+--
+-- Get the mover's current direction.
+-------------------------------------------------------------------------------
 function mover:getDir()
 	return self.dir
 end
 
+
+
+-------------------------------------------------------------------------------
+-- setExtent
+--
+-- Set one of the mover's extents.
+-------------------------------------------------------------------------------
 function mover:setExtent(i, vec)
 	self.extents[i] = vec;
 end
 
+
+
+-------------------------------------------------------------------------------
+-- getExtent
+--
+-- Get one of the mover's extents.
+-------------------------------------------------------------------------------
 function mover:getExtent(i)
 	return self.extents[i];
 end
 
+
+
+-------------------------------------------------------------------------------
+-- getCurrentExtent
+--
+-- Get the index of the mover's current extent.
+-------------------------------------------------------------------------------
 function mover:getCurrentExtent()
 	return self.currentExtent;
 end
 
+
+
+-------------------------------------------------------------------------------
+-- getOtherExtent
+--
+-- Get the index of the mover's non-current extent.
+-------------------------------------------------------------------------------
 function mover:getOtherExtent()
 	return self.otherExtent;
 end
 
+
+
+-------------------------------------------------------------------------------
+-- setCurrentExtent
+--
+-- Set the index of the mover's current extent.
+-------------------------------------------------------------------------------
 function mover:setCurrentExtent(e)
 	self.currentExtent = e
 end
 
+
+
+-------------------------------------------------------------------------------
+-- setOtherExtent
+--
+-- Set the index of the mover's non-current extent.
+-------------------------------------------------------------------------------
 function mover:setOtherExtent(e)
 	self.otherExtent = e
 end
 
+
+
+-------------------------------------------------------------------------------
+-- calcExtent
+--
+-- Calculate the proper values of the mover's extents.
+-------------------------------------------------------------------------------
 function mover:calcExtent(block)
 	self.extentsDist = self.extents[self.currentExtent]:dist(self.extents[self.otherExtent]) * block
 	self.dist = self.position:dist(self.extents[1] * block)
@@ -112,19 +243,26 @@ function mover:calcExtent(block)
 	self.extents[2] = self.extents[2] * block
 end
 
-function mover:setFirstExtent(ex)
-	self.extents[1] = ex
-end
 
-function mover:setSecondExtent(ex)
-	self.extents[2] = ex
-end
 
+-------------------------------------------------------------------------------
+-- move
+--
+-- Move the mover.
+-------------------------------------------------------------------------------
 function mover:move(pos)
 	self.oldPos.x, self.oldPos.y = self.position.x, self.position.y
 	gameObject.move(self, pos)
 end
 
+
+
+-------------------------------------------------------------------------------
+-- update
+--
+-- Updates the mover, reversing its direction and swapping its extents if it
+-- hits a said extent.
+-------------------------------------------------------------------------------
 function mover:update(dt) 
 	self.oldPos.x, self.oldPos.y = self.position.x, self.position.y
 	self.position.x = self.position.x + (self.dir.x * dt * self.speed)
@@ -134,7 +272,7 @@ function mover:update(dt)
 		if self.sounds["stomp"] then
 			--TEsound.play(self.sounds["stomp"].data)
 		end
-
+  
 		self.dir.x, self.dir.y = -self.dir.x, -self.dir.y
 		local tempVar = self.dist - self.extentsDist
 		self.position.x = self.extents[self.otherExtent].x + (self.dir.x * tempVar)
