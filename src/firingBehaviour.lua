fbcffc4a910b03b0892728cd1982aedd338e3d4c
Brad <brad@bradellis.co.uk>
2017-03-05 14:21:22 +0000

    Refactored firingBehaviour. This object should be very minimal in terms of state, since eventually all of its behaviour should be set in terms of its functions, which will be defined in individual script files. The init function has been emptied to bring it in line with other objects, whose init functions only set default values. Added header and comments.

--- src/firingBehaviour.lua
+++ src/firingBehaviour.lua
@@ -1,41 +1,69 @@
+-------------------------------------------------------------------------------
+-- Copyright (C) Brad Ellis 2013-2017
+--
+--
+-- firingBehaviour.lua
+--
+-- Object to contain gun behaviour.
+-------------------------------------------------------------------------------
+
 firingBehaviour = {}
 
 firingBehaviour.__index = firingBehaviour
 
-	setmetatable(firingBehaviour,
-			{__call = function(cls, ...)
-				return cls.new(...)
-			end})
+setmetatable(firingBehaviour,
+		{__call = function(cls, ...)
+      return cls.new(...)
+		end})
 
-	function firingBehaviour.new(...)
-		local self = setmetatable({}, firingBehaviour)
-		self:init(...)
-		return self
-	end
 
-	function firingBehaviour:init()
-		self.data = {}
-	end
 
-	function firingBehaviour:init(data, fireFunc, resetFunc, soundFunc)
-		self.data = data
-		self.fire = fireFunc
-		self.reset = resetFunc
-		self.isSoundReady = soundFunc
-	end
+function firingBehaviour:new(...)
+  local self = setmetatable({}, firingBehaviour)
+	self:init(...)
+	return self
+end
 
-	function firingBehaviour:fire(dt, gun)
-		print("No fire behaviour set")
-	end
 
-	function firingBehaviour:reset()
-		print("No reset behaviour set")
-	end
 
-	function firingBehaviour:setFireFunc(fireFunc)
-		self.fire = fireFunc
-	end
+-------------------------------------------------------------------------------
+-- init
+--
+-- Sets default values.
+-------------------------------------------------------------------------------
+function firingBehaviour:init()
+end
 
-	function firingBehaviour:setResetFunc(resetFunc)
-		self.reset = resetFunc
-	end
+
+
+-------------------------------------------------------------------------------
+-- fire
+--
+-- Fires bullets (or rather, tells bullets to fire themselves).
+-------------------------------------------------------------------------------
+function firingBehaviour:fire(dt)
+	print("No fire behaviour set")
+end
+
+
+
+-------------------------------------------------------------------------------
+-- setState
+--
+-- Sets the state - i.e. should the bullets fire or not?
+-------------------------------------------------------------------------------
+function firingBehaviour:setState(state)
+  self.state = state
+end
+
+
+
+-------------------------------------------------------------------------------
+-- setFireFunc
+--
+-- Sets the firing function - i.e. what should get called when we call "fire"?
+-------------------------------------------------------------------------------
+function firingBehaviour:setFireFunc(fireFunc)
+	self.fire = fireFunc
+end
+
