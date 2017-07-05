c560fdd600bd257a24b3edc35027814b79e0c793
Brad <brad@bradellis.co.uk>
2017-07-05 15:49:44 +0100

    Gave the gun object its own firingBehaviour functionality, so that behaviour has a per-gun component as well as a per-bullet component.

--- src/levels/scripts/gun_shotgun.lua
+++ src/levels/scripts/gun_shotgun.lua
@@ -20,24 +20,42 @@ local firingBehaviourFactory = {}
 -- holds the gun's properties from the level file.
 -------------------------------------------------------------------------------
 function firingBehaviourFactory:makeFiringBehaviour(data)
-  
   -- Make a generic gun
   local timeBetween = 2 -- one every 2 seconds
   local timeLastBullet = 0 -- time since gun last fired
-  
+
+  local updateGun = function(behaviour, dt)
+    timeLastBullet = timeLastBullet + dt
+  end
+
   -- fires bullets - or will, eventually
-  local fireFunc = function(behaviour, dt)
+  local updateBullet = function(behaviour, dt)
+    local ready = false
     if behaviour.state == "active" and timeLastBullet > timeBetween then
       -- fire a new bullet
       timeLastBullet = timeLastBullet - timeBetween
+      ready = true
     end
-    timeLastBullet = timeLastBullet + dt
+    
+    return ready
+  end
+  
+  local defaultVelocity = vector(15.0, 15.0)
+  
+  -- set initial position and velocity for the bullet
+  local calcInitialsFunc = function(behaviour, dt, gunPos, playerPos)
+    local velocity = defaultVelocity:clone()
+    local position = gunPos:clone()
+    
+    return position, velocity
   end
   
   -- create the object and pass it its closures/information
   local fbObject = firingBehaviour:new()
   
-  fbObject:setFireFunc(fireFunc)
+  fbObject:setUpdateBulletFunc(updateBullet)
+  fbObject:setUpdateGunFunc(updateGun)
+  fbObject:setCalcInitialsFunc(calcInitialsFunc)
   fbObject:setState("active")
   
   return fbObject;
