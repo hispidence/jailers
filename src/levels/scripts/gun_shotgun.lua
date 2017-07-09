2ba7284d2b3dbe9b6ccfa0c377e30d0c44584d74
Brad <brad@bradellis.co.uk>
2017-07-09 20:59:17 +0100

    gun_shotgun now loads more of its attributes from the level file generated by tiled. These include the angle, which is used to spread its bullets out, and the speed at which the bullets travel. A cleanup is planned for this file, as it's starting to look untidy and a good chunk of it looks reusable.

--- src/levels/scripts/gun_shotgun.lua
+++ src/levels/scripts/gun_shotgun.lua
@@ -21,21 +21,57 @@ local firingBehaviourFactory = {}
 -------------------------------------------------------------------------------
 function firingBehaviourFactory:makeFiringBehaviour(data)
   -- Make a generic gun
-  local timeBetween = 0.2 -- one every 2 seconds
   local timeLastBullet = 0 -- time since gun last fired
 
+  local timeBetween = data.secondsBetween
+  if 0 == timeBetween then
+    timeBetween = 0.5
+  end
+  
+  local bulletsPerShot = data.bulletsPerShot
+  if 0 == bulletsPerShot then
+    bulletsPerShot = 3
+  end
+  
+  local angle = data.firingAngle
+  if 0 == angle then
+    angle = 0.5
+  end
+  angle = math.rad(angle)
+  local radsPerShot = angle/(bulletsPerShot-1)
+  
+  local directionVectors = {}
+  
+  local startAngle = -angle/2;
+  
+  local originalDirVec = vector(data.directionX, data.directionY)
+  
+  for i = 1, bulletsPerShot do
+    directionVectors[i] = originalDirVec:rotated(startAngle + radsPerShot*(i-1))
+  end
+  
+  local bulletsAddedToShot = 0
+  local bulletsFired = 0
+  
   local updateGun = function(behaviour, dt, gunPos)
     self.gunPos = gunPos
     timeLastBullet = timeLastBullet + dt
+    bulletsAddedToShot = 0
+    bulletsFired = 0
   end
 
   -- fires bullets - or will, eventually
   local updateBullet = function(behaviour, dt)
     local ready = false
     if behaviour.state == "active" and timeLastBullet > timeBetween then
-      -- fire a new bullet
-      timeLastBullet = timeLastBullet - timeBetween
-      ready = true
+      -- fire a new bullet. max 5 per shot
+      if bulletsPerShot > bulletsAddedToShot then
+        bulletsAddedToShot = bulletsAddedToShot + 1
+        if bulletsPerShot == bulletsAddedToShot then
+            timeLastBullet = timeLastBullet - timeBetween
+        end
+        ready = true
+      end
     end
     
     return ready
@@ -49,14 +85,13 @@ function firingBehaviourFactory:makeFiringBehaviour(data)
     
     return killBullet
   end
-  
-  local defaultVelocity = vector(50.0, 0.0)
-  
+
+  local speed = data.speed
   -- set initial position and velocity for the bullet
-  local calcInitialsFunc = function(behaviour, dt)
-    local velocity = defaultVelocity:clone()
+  local calcInitialsFunc = function(behaviour)
     local position = self.gunPos:clone()
-    
+    bulletsFired = bulletsFired+1
+    local velocity = directionVectors[bulletsFired]*speed
     return position, velocity
   end
   
