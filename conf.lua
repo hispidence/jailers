3072128528230108af2b16cdf66d136c24b0eff1
Brad <brad.w.ellis.01@gmail.com>
2019-01-01 11:03:28 +0000

    Fixed header for conf.lua and removed references to the block size, which has been moved to gameConf.lua, and the window width/height, which is now set in gameMain.lua.

--- conf.lua
+++ conf.lua
@@ -1,19 +1,13 @@
 -------------------------------------------------------------------------------
--- Copyright (C) Brad Ellis 2013-2016
+-- Copyright (C) Brad Ellis 2013-2019
 --
 --
--- main.lua
+-- conf.lua
 --
--- Entry point for LÖVE
+-- LÖVE config.
 -------------------------------------------------------------------------------
 
-g_blockSize     = 16
-g_halfBlockSize = 8
-
 function love.conf(t)
-	t.title = "Jailers"
+  t.title = "Jailers"
   t.console = true
-	
-	t.window.width = (g_blockSize * 40)
-	t.window.height = (g_blockSize * 30)
 end
