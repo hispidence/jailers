75b768c2f56e44d6ccd6442dae15f56f6815f7d4
Brad <brad.w.ellis.01@gmail.com>
2019-01-01 10:50:45 +0000

    Added gameConf.lua. This is intended to hold some of the game's "const" properties (things which shouldn't be changed at runtime).

---
+++ src/gameConf.lua
@@ -0,0 +1,17 @@
+-------------------------------------------------------------------------------
+-- Copyright (C) Brad Ellis 2013-2019
+--
+--
+-- gameConf.lua
+--
+-- Game configuration things which aren't intended to be changed at runtime.
+-------------------------------------------------------------------------------
+
+local gameConf = {
+  widthInBlocks = 40,
+  heightInBlocks = 30,
+  scale = 2,
+  blockSize = 16
+}
+
+return gameConf
