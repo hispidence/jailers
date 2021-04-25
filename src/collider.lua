151e1d3320d9f7a45ec04e20272ecba50fe556d5
Brad Ellis <brad.w.ellis.01@gmail.com>
2021-04-25 22:01:08 +0100

    Added HC wrapper, collider.lua. Probably a stopgap but it's a cleaner stopgap than having it in a file where it's not used, at least.

---
+++ src/collider.lua
@@ -0,0 +1,12 @@
+-------------------------------------------------------------------------------
+-- Copyright (C) Hispidence 2013-2021
+--
+--
+-- collider.lua
+--
+-- Wrapper for HC
+-------------------------------------------------------------------------------
+local HC = require("src.external.HC")
+
+local theCollider = HC.new(128)
+return theCollider
